9a196993e6f771b87c9f28b03cfc59fe9a448d49
Leo Antunes <leocrantunes@gmail.com>
2020-02-07 09:47:40 -0500

    Initial commit with created project

---
+++ SQATA1Rectangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQATA1Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
