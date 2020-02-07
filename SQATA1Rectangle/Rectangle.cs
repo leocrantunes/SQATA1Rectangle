1fbe041b1294cf97ad35144f7554d0b11d52dc7b
Leo Antunes <leocrantunes@gmail.com>
2020-02-07 09:59:14 -0500

    Completed Rectangle class

---
+++ SQATA1Rectangle/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQATA1Rectangle
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public void SetLength(int length) 
+        {
+            this.length = length;
+        }
+        
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public void SetWidth(int width) 
+        { 
+            this.width = width; 
+        }
+        
+        public int GetPerimeter() 
+        {
+            return 2 * (length + width);
+        }
+        
+        public int GetArea() 
+        {
+            return length * width;
+        }
+    }
+}
